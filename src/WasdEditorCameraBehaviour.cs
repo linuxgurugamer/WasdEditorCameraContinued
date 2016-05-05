636ad1911642db6cda0e0e51395832f9dd05bbca
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-05-05 09:38:10 -0400

    Updated version file

--- src/WasdEditorCameraBehaviour.cs
+++ src/WasdEditorCameraBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-using System.Reflection;
+//using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace WasdEditorCamera
